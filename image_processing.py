bbfda5d0b17edefef2765cd319c03bb550a279bf
Nathan <nathancorwin2015@u.northwestern.edu>
2015-09-15 11:43:49 -0500

    added image_processing.py for testing image processing code -Nathan

---
+++ image_processing.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import rospy
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+
+
+def main():
+
+    return()
+
+
+if __name__ == '__main__':
+    main()
