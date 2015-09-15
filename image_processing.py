1f40f1e0b5b8985e862c01f2b1deec13cf8d69de
Nathan <nathancorwin2015@u.northwestern.edu>
2015-09-15 15:38:07 -0500

    updated launch and still working on image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -6,45 +6,53 @@ import sys
 import rospy
 import cv2
 from std_msgs.msg import String
-from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
-class image_converter:
-    
+class test_vision_node:
+
     def __init__(self):
-        self.image_pub = rospy.Publisher("image_topic_2",Image)
+        rospy.init_node('test_vision_node')
 
-        cv2.namedWindow("Image window", 1)
+        """ Give the OpenCV display window a name. """
+        self.cv_window_name = "OpenCV Image"
+
+        """ Create the window and make it re-sizeable (second parameter = 0) """
+        cv2.namedWindow(self.cv_window_name, 0)
+
+        """ Create the cv_bridge object """
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("image_raw",Image,self.callback)
 
-    def callback(self,data):
+        """ Subscribe to the raw camera image topic """
+        self.image_sub = rospy.Subscriber("/usb_cam/image_raw", Image, self.callback)
+
+    def callback(self, data):
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            """ Convert the raw image to OpenCV format """
+            cv_image = self.bridge.imgmsg_to_cv(data, "bgr8")
         except CvBridgeError, e:
-            print e
+          print e
+  
+        
+        """ Get the width and height of the image """
+        (width, height) = cv.GetSize(cv_image)
 
-    (rows,cols,channels) = cv_image.shape
-    if cols > 60 and rows > 60 :
-        cv2.circle(cv_image, (50,50), 10, 255)
-
-    cv2.imshow("Image window", cv_image)
-    cv2.waitkey(3)
-
-    try:
-        self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-    except CvBridgeError, e:
-        print e
+        """ Overlay some text onto the image display """
+        text_font = cv2.InitFont(cv2.CV_FONT_HERSHEY_DUPLEX, 2, 2)
+        cv2.putText(cv_image, "OpenCV Image", (50, height / 2), text_font, cv.RGB(255, 255, 0))
+  
+        """ Refresh the image on the screen """
+        cv2.showImage(self.cv_window_name, cv_image)
+        cv2.waitKey(3)
 
 def main(args):
-    ic = image_converter()
-    rospy.init_node('image_converter', anonymous=True)
-    try:
+      vn = test_vision_node()
+      try:
         rospy.spin()
-    except KeyboardInterrupt:
-        print "Shutting Down"
-    cv2.destroyAllWindows()
-
+      except KeyboardInterrupt:
+        print "Shutting down vison node."
+      cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
+    
