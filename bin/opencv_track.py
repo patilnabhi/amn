636954b7f267bc00c83a1f537235d453af1272a8
Abhishek <patilnabhi@gmail.com>
2015-09-16 08:12:13 -0500

    Modified opencv.py with ball_tracking using Hough Transform
    Modified camera.launch to work with opencv.py file

---
+++ bin/opencv_track.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import roslib
+roslib.load_manifest('ball_tracking')
+import sys
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np 
+import argparse
+
+class image_converter:
+
+  def __init__(self):
+    # rospy.init_node('image_converter')
+
+    self.image_pub = rospy.Publisher("image_topic_2",Image,queue_size=10)
+
+    cv2.namedWindow("Image window", 1)
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
+
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError, e:
+      print e
+
+    # ap = argparse.ArgumentParser()
+    # ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+    # args = vars(ap.parse_args())
+
+    # image = cv2.imread(usb_cam)
+    output = cv_image.copy()
+    gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+
+    # detect circles in the image
+    circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100)
+ 
+    # ensure at least some circles were found
+    if circles is not None:
+      # convert the (x, y) coordinates and radius of the circles to integers
+      circles = np.round(circles[0, :]).astype("int")
+ 
+    # loop over the (x, y) coordinates and radius of the circles
+      for (x, y, r) in circles:
+        # draw the circle in the output image, then draw a rectangle
+        # corresponding to the center of the circle
+        cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+        cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+ 
+    # show the output image
+    cv2.imshow("output", np.hstack([cv_image, output]))
+    # cv2.waitKey(0)
+
+    # cv2.imshow("Image window", cv_image)
+    cv2.waitKey(3)
+
+    try:
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+    except CvBridgeError, e:
+      print e
+
+def main(args):
+  rospy.init_node('image_converter', anonymous=True)
+  ic = image_converter()
+  
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print "Shutting down"
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
